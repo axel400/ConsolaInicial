caa4936f9951f966d0756f6976c4dd5ee8b8e334
axel400 <70916648+axel400@users.noreply.github.com>
2021-06-23 20:41:51 -0500

    Paises
    Trabajo en clase.

---
+++ SegundaSemana/Canton.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+namespace SegundaSemana
+{
+    class Canton
+    {
+        public int CantonId
+        {
+            get; set;
+        }
+        public string Nombre
+        {
+            get; set;
+        }
+        public List<Parroquia> Parroquias
+        {
+            get; set;
+        } = new List<Parroquia>();
+        public int ObtenerNumeroDeHabitantes()
+        {
+            int acumulador = 0;
+            foreach (Parroquia actual in Parroquias)
+            {
+                acumulador = acumulador + actual.Habitantes;
+            }
+            return acumulador;
+        }
+    }
+
+
+}
---
+++ SegundaSemana/Parroquia.cs
@@ -0,0 +1,20 @@
+namespace SegundaSemana
+{
+    class Parroquia
+    {
+        public int ParroquiaId
+        {
+            get; set;
+        }
+        public string Nombre
+        {
+            get; set;
+        }
+        public int Habitantes
+        {
+            get; set;
+        }
+    }
+
+
+}
