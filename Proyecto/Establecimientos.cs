28819e0b3311c2e27cdcbf49b9250606cde9e103
axel400 <70916648+axel400@users.noreply.github.com>
2021-07-04 20:55:25 -0500

    Proyecto
    Proyecto Integrador

---
+++ Proyecto/Establecimientos.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto
+{
+    class Establecimientos
+    {
+        public double cantidad;
+        public string nombreEstableci;
+        public int nit;
+        public string barrio;
+        public string serviciosOfrece;
+        public string jornada;
+        public int cantidadEmpleadosActuales;
+        public int cantidadEmpleadosDespedidos;
+        public double perdidaDineroPersona;
+        public double bono = 50000;
+        public double perdida = 150000, total, totalPerdida;
+        int opc;
+        public double acu = 0;
+        double perd1 = 0, ttP1 = 0;
+        double ttD =0, Ttp = 0, tt1 =0, tt2 = 0;
+
+    }
+}
---
+++ Proyecto/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace Proyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            manu.Mostrar();
+        }
+    }
+}
